a9ea58606a317fa1bc21cc37abca893bf34967a0
Mo <hamzaniaz9856@gmail.com>
2026-08-18 17:24:24 -0400

    Fixed ComponentScan Packages format

--- ExpenseReimbursementSystemIteration2/src/main/java/com/rev/g3/i2/ers2/AppConfig.java
+++ ExpenseReimbursementSystemIteration2/src/main/java/com/rev/g3/i2/ers2/AppConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "com/rev/g3/i2/ers2/*")
+@ComponentScan(basePackages = "com.rev.g3.i2.ers2.*")
 public class AppConfig {
 
 }
