658bcb8357fa8fb3c87b06cc0515254bf81c45a2
user.email <c20cabonilasc@gmail.com>
2026-08-18 16:52:37 -0400

    It begins

---
+++ ExpenseReimbursementSystemIteration2/src/main/java/com/rev/g3/i2/ers2/App.java
@@ -0,0 +1,12 @@
+package com.rev.g3.i2.ers2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
---
+++ ExpenseReimbursementSystemIteration2/src/test/java/com/rev/g3/i2/ers2/ExpenseReimbursementSystemIteration2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rev.g3.i2.ers2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseReimbursementSystemIteration2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
