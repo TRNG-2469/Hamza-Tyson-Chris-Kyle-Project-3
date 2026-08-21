2dd4b84918d4921ca77320a5205dc97b929e2dfb
user.email <c20cabonilasc@gmail.com>
2026-08-21 16:35:33 -0400

    Refactored DepartmentHandler to use RestController functionality

--- ExpenseReimbursementSystemIteration2/src/main/java/com/rev/g3/i2/ers2/controller/DepartmentHandler.java
+++ ExpenseReimbursementSystemIteration2/src/main/java/com/rev/g3/i2/ers2/controller/DepartmentHandler.java
@@ -1,8 +1,27 @@
 package com.rev.g3.i2.ers2.controller;
 
-import io.javalin.http.Context;
+import com.rev.g3.i2.ers2.model.Department;
+import com.rev.g3.i2.ers2.service.DepartmentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-public interface DepartmentHandler {
-    void findDepartmentById(Context ctx);
-    void findAll(Context ctx);
+import java.util.List;
+
+@RestController
+public class DepartmentHandler {
+    private final DepartmentService departmentService;
+
+    public DepartmentHandler(DepartmentService departmentService) {
+        this.departmentService = departmentService;
+    }
+
+    @GetMapping("/departments/{id}")
+    public ResponseEntity<Department> findDepartmentById(@PathVariable int id) {
+        return ResponseEntity.ok().body(departmentService.findDepartmentById(id));
+    }
+
+    @GetMapping("/departments")
+    public ResponseEntity<List<Department>> findAll() {
+        return ResponseEntity.ok().body(departmentService.findAll());
+    }
 }
--- ExpenseReimbursementSystemIteration2/src/main/java/com/rev/g3/i2/ers2/controller/UserHandler.java
+++
@@ -1,9 +0,0 @@
-package com.rev.g3.i2.ers2.controller;
-
-import io.javalin.http.Context;
-
-public interface UserHandler {
-    public void login(Context ctx);
-    public void register(Context ctx);
-    public void logout(Context ctx);
-}
