c850dd642a884509539a039f6929b467829b55a6
Thien Pham <forward.run.9@gmail.com>
2026-08-24 14:44:12 -0400

    created a jwt service class that handle signing and validating token as well as getting payload

--- ExpenseReimbursementSystemIteration2/src/main/java/com/rev/g3/i2/ers2/config/SecurityConfig.java
+++ ExpenseReimbursementSystemIteration2/src/main/java/com/rev/g3/i2/ers2/config/SecurityConfig.java
@@ -1,9 +1,18 @@
 package com.rev.g3.i2.ers2.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 
 
 }
--- ExpenseReimbursementSystemIteration2/src/main/java/com/rev/g3/i2/ers2/dto/AuthResponse.java
+++ ExpenseReimbursementSystemIteration2/src/main/java/com/rev/g3/i2/ers2/dto/AuthResponse.java
@@ -11,9 +11,34 @@ public class AuthResponse {
     private Role role;
 
     public AuthResponse(String token, String username, Role role ) {
+        this.token = token;
+        this.username = username;
+        this.role = role;
+
 
     }
 
+    public String getToken() {
+        return token;
+    }
 
+    public void setToken(String token) {
+        this.token = token;
+    }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
