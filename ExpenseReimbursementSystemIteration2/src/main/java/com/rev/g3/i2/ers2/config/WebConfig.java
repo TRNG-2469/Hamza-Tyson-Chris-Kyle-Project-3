62d27997d946b96085e6f8161ef3f95753068a49
Thien Pham <forward.run.9@gmail.com>
2026-08-27 18:46:35 -0400

    logout fix

--- ExpenseReimbursementSystemIteration2/src/main/java/com/rev/g3/i2/ers2/config/WebConfig.java
+++ ExpenseReimbursementSystemIteration2/src/main/java/com/rev/g3/i2/ers2/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.rev.g3.i2.ers2.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -9,7 +10,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // any request for /<name>.html gets served from classpath:/pages/<name>.html
+
         registry.addResourceHandler("/*.html")
                 .addResourceLocations("classpath:/pages/");
+    }
+// redirct when logout
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // logout go straight to the login page
+        registry.addRedirectViewController("/", "/login.html");
     }
 }
