010606970c36861b8cb4ba73543c51dfe693104b
Thien Pham <forward.run.9@gmail.com>
2026-08-27 12:27:52 -0400

    view all pending reimbursement done and manager can resolve reimbursement from front end now

--- ExpenseReimbursementSystemIteration2/src/main/java/com/rev/g3/i2/ers2/model/User.java
+++ ExpenseReimbursementSystemIteration2/src/main/java/com/rev/g3/i2/ers2/model/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -15,7 +16,7 @@ import java.util.Objects;
 @Table(name="Users")
 @Data
 @NoArgsConstructor
-
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
