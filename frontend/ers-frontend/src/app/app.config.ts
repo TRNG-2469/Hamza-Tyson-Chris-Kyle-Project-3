8ef871ee1a637da81d926f27ce7b98aa6a799f6f
Thien Pham <forward.run.9@gmail.com>
2026-09-14 16:51:10 -0400

    wired up HttpClient allowing HttpClient to be injected into any services

--- frontend/ers-frontend/src/app/app.config.ts
+++ frontend/ers-frontend/src/app/app.config.ts
@@ -1,10 +1,14 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    // register angular HttpClient service so that it can be injected into other services
+    provideHttpClient()
   ]
 };
