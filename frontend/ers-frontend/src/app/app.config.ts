89b09db6aa1566c5a4c4aaa4f77c78303bc99b91
Thien Pham <forward.run.9@gmail.com>
2026-09-14 17:42:42 -0400

    update app config to register the interceptor

--- frontend/ers-frontend/src/app/app.config.ts
+++ frontend/ers-frontend/src/app/app.config.ts
@@ -1,7 +1,10 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
+
+
 
 
 export const appConfig: ApplicationConfig = {
@@ -9,6 +12,6 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     // register angular HttpClient service so that it can be injected into other services
-    provideHttpClient()
+    provideHttpClient(withInterceptors([authInterceptor])),
   ]
 };
