0b9910d00edafec507e87deb7f98efd6307f5d7b
Thien Pham <forward.run.9@gmail.com>
2026-09-15 18:03:32 -0400

    create deparment  service to get departments from backend

---
+++ frontend/ers-frontend/src/app/core/services/department.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { API_BASE_URL } from '../config/api-config';
+import { Department } from '../models/department.model';
+
+// department service to get department data from the backend
+@Injectable({providedIn: 'root'})
+export class DepartmentService {
+    // inject HttpClient to make HTTP request ie GET,POST, ETC
+    constructor(private http: HttpClient) {
+
+    }
+    // get all method hit the GET/departments endpoint
+    gettAll(): Observable<Department[]> {
+        // return the array of departments from the backend using the HttpClient GET
+        return this.http.get<Department[]>(`${API_BASE_URL}/departments`);
+}
+
+
