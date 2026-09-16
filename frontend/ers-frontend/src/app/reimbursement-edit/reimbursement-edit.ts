98a205bc518a4abfdc72bb72115f971d87bfc75a
user.email <c20cabonilasc@gmail.com>
2026-09-16 17:32:20 -0400

    Incomplete changes for the edit panel and some dashboard work

--- frontend/ers-frontend/src/app/reimbursement-edit/reimbursement-edit.ts
+++ frontend/ers-frontend/src/app/reimbursement-edit/reimbursement-edit.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Reimbursement } from '../reimbursement';
 
 @Component({
   imports: [],
   selector: 'app-reimbursement-edit',
   styleUrl: './reimbursement-edit.css',
   templateUrl: './reimbursement-edit.html',
+  standalone: true,
 })
 export class ReimbursementEdit {
+  @Input() selectedReimbursement: Reimbursement | null = null;
+
+  @Output()
+  editStarted: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  submitEdits() {
+    this.editStarted.emit(false);
+  }
+
+  cancelEdits() {
+    this.editStarted.emit(false);
+  }
+
+  @Output() public amount = new EventEmitter();
+  @Output() public desc = new EventEmitter();
+  @Output() public type = new EventEmitter();
 }
--- frontend/ers-frontend/src/app/reimbursement-list-container/reimbursement-list-container.ts
+++ frontend/ers-frontend/src/app/reimbursement-list-container/reimbursement-list-container.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { ReimbursementList } from '../reimbursement-list/reimbursement-list';
 import { ReimbursementDetailsPanel } from '../reimbursement-details-panel/reimbursement-details-panel';
 import { Reimbursement } from '../reimbursement';
+import { ReimbursementEdit } from '../reimbursement-edit/reimbursement-edit';
+import { AuthService } from '../core/services/auth.service';
 
 @Component({
-  imports: [ReimbursementList, ReimbursementDetailsPanel],
+  imports: [ReimbursementList, ReimbursementDetailsPanel, ReimbursementEdit],
   selector: 'app-reimbursement-list-container',
   styleUrl: './reimbursement-list-container.css',
   templateUrl: './reimbursement-list-container.html',
 })
 export class ReimbursementListContainer {
+    authService: AuthService = Inject(AuthService);
 
     selectedReimbursement: Reimbursement | null = null;
+    startedEdit: boolean = false;
 
     handleSelectReimbursement(reimbursement: Reimbursement) {
         this.selectedReimbursement = reimbursement;
