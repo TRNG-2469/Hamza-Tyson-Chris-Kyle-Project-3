1f6e68208c758450836ce40fa389a92052b6a6d5
user.email <c20cabonilasc@gmail.com>
2026-09-16 20:13:35 -0400

    Created component for reimbursement submission

---
+++ frontend/ers-frontend/src/app/reimbursement-submission/reimbursement-submission.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-reimbursement-submission',
+  styleUrl: './reimbursement-submission.css',
+  templateUrl: './reimbursement-submission.html',
+})
+export class ReimbursementSubmission {}
